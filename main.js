6b314d0c617be3b48ee197e8f5f4cfaedf8d5a3a
mccrone-ai <scottmccrone@hotmail.co.uk>
2021-04-16 19:49:40 +0100

    Chuck Norris First Commit

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
+let baseUrl = "https://api.chucknorris.io/jokes/random"
+let displayJoke = document.querySelector(".display-facts");
+
+async function getChuckle(){
+    const response = await fetch (baseUrl);
+    const data = await response.json();
+    const joke = data.value;
+    displayJoke.innerHTML = joke;
+}
+
 
